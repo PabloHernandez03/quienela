e1cd0ce5270f53d225d7d231cb5e0bfab881ad25
PabloHernandez03 <pablohernandez6287@alumnos.udg.mx>
2025-02-02 21:42:02 -0600

    a unexpecable mistake

--- src/router.tsx
+++ src/router.tsx
@@ -3,11 +3,13 @@ import AppLayout from './layouts/AppLayout'
 import DashBoardView from './views/DashBoardViews'
 
 export default function Router() {
+    const selectedDate = new Date(); // or any logic to get the selected date
+
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<AppLayout />}>
-                    <Route path="/" element={<DashBoardView />} index/>
+                    <Route path="/" element={<DashBoardView selectedDate={selectedDate} />} index/>
                 </Route>
             </Routes>
         </BrowserRouter>
